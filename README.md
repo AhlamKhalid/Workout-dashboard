460a82a70b957315eeab4fe09b368c25a92b1e6c
Ahlam <ahlamkarkashan@gmail.com>
2020-09-05 00:28:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Build workout tracker dashboard
+
+Converting a design of workout tracker dashboard into HTML and SASS. There is an ability to switch between light/dark themes. The dashboard is fully responsive.
+
+## Contents
+
+- [Demo](#Demo)
+- [Run project](#Run-project)
+- [Technologies](#Technologies)
+- [Resources](#Resources)
+
+### Demo
+
+![demo](demo.gif)
+
+## Run project
+
+Open up `index.html` in your browser.
+
+### Technologies
+
+- Design built with Figma
+- Building the dashboard with HTML & SASS
+
+### Resources
+
+- Iconify icons of Figma
+- [User picture: Photo by Kelly Sikkema on Unsplash](https://unsplash.com/photos/w45roKo6QYw)
+- Inspiration of design on Dribbble: [Dark Dashboard](https://dribbble.com/shots/7183165-Dark-Dashboard), [Light Dashboard](https://dribbble.com/shots/7183367-Dashboard).
---
+++ main.js
@@ -0,0 +1,38 @@
+// variables
+const sideNavWidth = "200px";
+
+// elements
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const sideNav = document.querySelector("#side-nav");
+const body = document.querySelector("body");
+const sunIcons = document.querySelectorAll(".sun");
+const moonIcons = document.querySelectorAll(".moon");
+
+// event listeners
+
+/* open side nav */
+openMenu.addEventListener("click", () => {
+  sideNav.style.right = "0";
+  body.style.left = `-${sideNavWidth}`;
+});
+
+/* close side nav */
+closeMenu.addEventListener("click", () => {
+  sideNav.style.right = `-${sideNavWidth}`;
+  body.style.left = "0";
+});
+
+// theme toggler: outlined/filled sun
+for (const sun of sunIcons) {
+  sun.addEventListener("click", () => {
+    body.classList.remove("dark");
+  });
+}
+
+// theme toggler: outlined/filled moon
+for (const moon of moonIcons) {
+  moon.addEventListener("click", () => {
+    body.classList.add("dark");
+  });
+}
